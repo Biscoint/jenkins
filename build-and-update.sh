5de16dadff34630ab42f2c75ca1324ae6995ac8e
root <jonathascarrijo@gmail.com>
2018-07-19 22:09:02 +0000

    Add jks as external volume

--- build-and-update.sh
+++ build-and-update.sh
@@ -5,4 +5,4 @@ docker rm jenkins
 
 docker build . -t jonathascarrijo/jenkins:local
 
-docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx256m -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins:local &
+docker run -p 8080:8080 -p 50000:50000 -v ~/biscoint-apk-keystore.jks:/opt/biscoint-apk-keystore.jks -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx256m -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins:local &
