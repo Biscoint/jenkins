e8278ec2cda1d27430a699908f5c4fef8a21f4ac
Jonathas Carrijo <jonathascarrijo@gmail.com>
2018-10-25 12:15:46 -0300

    Add keystore mapping to run_jenkins.sh

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -1,3 +1,3 @@
 # docker stop jenkins
 # docker rm jenkins
-docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx256m -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins-meteor:latest &
+docker run -p 8080:8080 -p 50000:50000 -v ~/biscoint-apk-keystore.jks:/opt/biscoint-apk-keystore.jks -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx256m -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins-meteor:latest &
