c3c668f7edd3b25101da74b0c9e6c5399cb16c10
Thiago Borges Abdnur <bolaum@gmail.com>
2019-02-27 10:47:43 -0300

    Increase jvm available memory

--- build-and-update.sh
+++ build-and-update.sh
@@ -1,8 +1,7 @@
 git pull origin master
 
-docker stop jenkins
-docker rm jenkins
+./rm_jenkins.sh
 
 docker build . -t jonathascarrijo/jenkins:local
 
-docker run -p 8080:8080 -p 50000:50000 -v ~/biscoint-apk-keystore.jks:/opt/biscoint-apk-keystore.jks -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx256m -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins:local &
+docker run -p 8080:8080 -p 50000:50000 -v ~/biscoint-apk-keystore.jks:/opt/biscoint-apk-keystore.jks -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx2g -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins:local &
