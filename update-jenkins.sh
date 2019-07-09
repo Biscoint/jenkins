c3c9172c5521a58b1b50fef450acab7af3a71c38
Jonathas Carrijo <jonathascarrijo@gmail.com>
2019-07-09 15:22:14 -0300

    Update update-jenkins.sh

--- update-jenkins.sh
+++ update-jenkins.sh
@@ -4,4 +4,4 @@ docker pull jonathascarrijo/jenkins-meteor:latest
 
 ./rm_jenkins.sh
 
-docker run -p 8080:8080 -p 50000:50000 -v ~/biscoint-apk-keystore.jks:/opt/biscoint-apk-keystore.jks -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx2g -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins-meteor:latest &
+./run_jenkins.sh
