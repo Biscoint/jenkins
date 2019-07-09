cef288cc381d54f2f9b7cd55c4ca354103d4fd64
Jonathas Carrijo <jonathascarrijo@gmail.com>
2019-07-09 15:20:06 -0300

    Update build-and-update.sh

--- build-and-update.sh
+++ build-and-update.sh
@@ -4,4 +4,4 @@ git pull origin master
 
 ./build_jenkins_image.sh
 
-docker run -p 8080:8080 -p 50000:50000 -v ~/biscoint-apk-keystore.jks:/opt/biscoint-apk-keystore.jks -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx2g -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins-meteor:local &
+./run_jenkins.sh
