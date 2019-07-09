f35e1f56aecc65dfd7f0a9a94678ac7f1141d3e7
Jonathas Carrijo <jonathascarrijo@gmail.com>
2019-07-09 15:17:59 -0300

    Update build-and-update.sh

--- build-and-update.sh
+++ build-and-update.sh
@@ -2,6 +2,6 @@ git pull origin master
 
 ./rm_jenkins.sh
 
-docker build . -t jonathascarrijo/jenkins:local
+docker build . -t jonathascarrijo/jenkins-meteor:local
 
-docker run -p 8080:8080 -p 50000:50000 -v ~/biscoint-apk-keystore.jks:/opt/biscoint-apk-keystore.jks -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx2g -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins:local &
+docker run -p 8080:8080 -p 50000:50000 -v ~/biscoint-apk-keystore.jks:/opt/biscoint-apk-keystore.jks -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --env JAVA_OPTS="-Xmx2g -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=900" --name jenkins -d jonathascarrijo/jenkins-meteor:local &
